df105edcc93308348f5d1d7c69417e99b12506d0
naonao708 <naonao708@outlook.com>
2022-04-19 20:31:37 +0900

    4/19

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -13,7 +13,19 @@ const TaskItem: React.FC<Props> = ({ task, handleDone, handleDelete }) =>  {
         <li className={task.done ? 'done' : ''}>
             <label>
                 <input 
-                    type="checkbox" />
+                    type="checkbox" 
+                    className="checkbox-input"
+                    onClick={ () => handleDone(task)}
+                    defaultChecked={task.done}
+                />
+                <span className="checkbox-label">{ task.title }</span>
             </label>
+            <button
+                onClick={ () => handleDelete(task)}
+                className="btn is-delete"
+            >Delete</button>
+        </li>
     )
 }
+
+export default TaskItem
